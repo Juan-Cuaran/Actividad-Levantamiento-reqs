7452eeb33618a97a0dfd14d92bcf5176fbe550a4
= <=>
2025-09-16 13:01:24 -0500

    GUI

--- interfaz.py
+++ interfaz.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 from main import CafeteriaCampus
-from Pedido import Pedido
+from Pedido import Pedido, Tipo_cliente
 from producto import Producto
 
 
@@ -17,20 +17,24 @@ class CafeteriaGUI:
     def GUI (self):
         frame_h = tk.Frame(self.ventana, bg="cyan", height=80)
         frame_h.pack()
+
         label_h = tk.Label(self.ventana, text="CAFETERIA CAMPUS", font=('Arial', 40, "bold"), bg="cyan")
         label_h.pack()
+        
+        self.entrada = tk.Entry(self.ventana, font=("Consolas", 20), justify="center")
+        self.entrada.pack()
 
         boton_cliente = tk.Button(self.ventana, text="Seleccione el tipo de cliente")
         boton_cliente.config(fg="White", bg ="light grey",font=("Arial", 20), command=self.Seleccionar_cliente())
 
-
-    
-
     def Seleccionar_cliente (self):
         self.pedido = Pedido()
+        respuesta = int(self.entrada.get())
+
+        if respuesta == 1:
+            self.pedido.tipo_cliente = Tipo_cliente.ESTUDIANTE
         
-        boton_Estudiante = tk.Button(self.ventana, text="Estudiante")
-        boton_Estudiante.config(fg="White", bg="light grey", font=("Arial", 20))
+
 
 
 
